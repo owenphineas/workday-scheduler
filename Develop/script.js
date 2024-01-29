081002020945f1c950cd899ab19414557a60a4bc
Owen Johnson <opjoh@gmail.com>
2024-01-29 16:14:55 -0600

    added save function

--- Develop/script.js
+++ Develop/script.js
@@ -1,16 +1,20 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
 $(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
 
   let container = $(".container-fluid");
+
+  // Adds an event listener for each save button
+  function saveText() {
+  for(i = 0; i < 16; i++) {
+    let btn = $(container.children().eq(i).children().eq(2));
+    btn.on("click", function() {
+      console.log(btn.prev().val());
+      // Checks the value of the clicked save button's corresponding text field and saves the contents in localStorage
+      localStorage.setItem(JSON.stringify(btn.parent().attr("id")), btn.prev().val());
+    })
+  };
+  }
+  saveText();
+
 
   let currentHour = dayjs().hour();
   console.log(currentHour);
